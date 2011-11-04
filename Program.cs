d994dd0d1f24b357db599d4d26aeba8531633065
Max Putilov <max.putilov@gmail.com>
2011-11-04 20:28:05 +0300

    first commit

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Coffee_Watcher
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+        }
+    }
+}
